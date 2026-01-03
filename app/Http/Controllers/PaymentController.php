cc48ea3492c26e2bbad3384dd17c7591b6a6ca48
Harshdeepchandra <81947916+harshdeepchandra@users.noreply.github.com>
2026-01-03 13:12:25 +0530

    Add files via upload

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Payment;
+use App\Models\Enrollment;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+class PaymentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index():View
+    {
+        $payments = Payment::all();
+        return view ('payments.index')->with('payments', $payments);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create():View
+    {
+        $enrollments=Enrollment::pluck('enroll_no','id');
+        return view('payments.create',compact('enrollments'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request):RedirectResponse
+    {
+        $input = $request->all();
+        Payment::create($input);
+        return redirect('payments')->with('flash_message', 'Payment Addedd!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id):View
+    {
+        $payments = Payment::find($id);
+        return view('payments.show')->with('payments', $payments);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id):View
+    {
+        $payments = Payment::find($id);
+        return view('payments.edit')->with('payments', $payments);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): RedirectResponse
+    {
+        $payments = Payment::find($id);
+        $input = $request->all();
+        $payments->update($input);
+        return redirect('payments')->with('flash_message', 'Payment Updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id): RedirectResponse
+    {
+        Payment::destroy($id);
+        return redirect('payments')->with('flash_message', 'Payment deleted!');
+    }
+}
